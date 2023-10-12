89576936474d6b63f08a6b26decffa9ef0bbf917
trjia <147029840+trjia@users.noreply.github.com>
2023-10-12 07:21:56 +0000

    1210(1)

--- s00_bailam.py
+++ s00_bailam.py
@@ -33,6 +33,12 @@ hi('1', '22', '333', '4444') | Hi 1, 22, 333 and 4444!
 #endregion debai
 
 #region bailam
-def hi(name):
-  return 'todo'
+def hi(name=None):
+ if name is None or name == '':
+   return f'Hi!'
+ else: return f'Hi {name}!'
+hi(name='Mom')
+hi('Mom')
+hi('')
+hi(None)
 #endregion bailam
